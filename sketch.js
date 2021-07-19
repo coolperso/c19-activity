7a9818d32273345b0fb5cf9a140f5899f0a01492
coolperso <86847699+coolperso@users.noreply.github.com>
2021-07-19 10:30:47 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+var s1,s2
+function setup() {
+  createCanvas(400,400);
+  s1=new Student("Sid","15","9")
+  s1.display()
+  s2=new Student("Shiv",15,9)
+  s2.display()
+
+}
+
+function draw() 
+{
+  background(30);
+  
+}
